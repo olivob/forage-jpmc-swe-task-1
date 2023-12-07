b039748fe734bc005191bd7ed223bcb9ef540ece
Bryan Olivo <bryanolivo@Bryans-Air-4.lan>
2023-12-06 17:52:55 -0800

    getDataPoint returns correct tuple, getRatio returns correct ratio, basic unit tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -9,11 +10,19 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
 
+    for quote in quotes:
+       askPrice, bidPrice = quote['top_ask']['price'], quote['top_bid']['price']
+       self.assertEqual(getRatio(askPrice, bidPrice), askPrice/bidPrice)
+
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+
+    for quote in quotes:
+       askPrice, bidPrice = quote['top_ask']['price'], quote['top_bid']['price']
+       self.assertEqual(getRatio(askPrice, bidPrice), askPrice/bidPrice)
     """ ------------ Add the assertion below ------------ """
 
 
